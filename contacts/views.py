841073a3dd441bb8ae191ce8095c9740f68c3d32
kbbushman <k.bushman@yahoo.com>
2018-10-19 11:11:23 -0700

    Solution up to auth

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Import Contact Model
+from .models import Contact
+
+def contacts_index(request):
+	contacts = Contact.objects.all()
+	return render(request, 'contacts/contacts_index.html', {'contacts': contacts})
+
+def contacts_show(request, contact_id):
+	contact = Contact.objects.get(id=contact_id)
+	return render(request, 'contacts/contacts_show.html', {'contact': contact})
