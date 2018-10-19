f01cd5dcaa255085b8b0b402ca6525485f4bb86d
kbbushman <k.bushman@yahoo.com>
2018-10-19 13:40:54 -0700

    Adds accounts app

--- contacts_project/urls.py
+++ contacts_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('contacts/', include('contacts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
 ]
